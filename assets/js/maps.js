33d42c5c4004d66758e241d8dbac2ad45a817ef7
JimEv87 <jimevans212@gmail.com>
2020-09-08 12:11:24 +0000

    map locations corrected

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,18 +1,18 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 6,
     center: {
-      lat: 46.619261,
-      lng: -33.134766,
+      lat: 52.821868,
+      lng: -2.925709,
     },
   });
 
   var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   var locations = [
-    { lat: 40.785091, lng: -73.968285 },
-    { lat: 41.084045, lng: -73.874245 },
-    { lat: 40.754932, lng: -73.984016 },
+    { lat: 52.857769, lng: -3.051070 },
+    { lat: 52.683519, lng: -2.434084 },
+    { lat: 53.050680, lng: -3.011651 },
   ];
 
   var markers = locations.map(function (location, i) {
